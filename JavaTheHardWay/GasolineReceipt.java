f133606e7375a57b2c2fd947dfe98eb093f56bad
AlexWagner <alexwagner1990@verizon.net>
2018-02-18 18:24:06 -0700

    Finish JTHW ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,15 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      CORNER STORE      |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| 2018-02-18  04:38PM    |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Gallons:       10.870  |" );
+    System.out.println( "| Price/gallon: $ 2.436  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel total: $  26.48   |" );
+    System.out.println( "+------------------------+" );
+  }
+}
